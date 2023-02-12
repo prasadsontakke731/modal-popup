9f2b12ae69951513b06c6eb5cf9cb28487beac62
prasad anant sontakke <prasadsontakke731@gmail.com>
2023-02-12 16:31:11 +0530

    modal popup

---
+++ main.js
@@ -0,0 +1,10 @@
+let popup = document.querySelector('.popup');
+console.log(popup);
+
+function openpopup() {
+  popup.classList.add('open-popup');
+}
+
+function closepopup() {
+  popup.classList.remove('open-popup');
+}
